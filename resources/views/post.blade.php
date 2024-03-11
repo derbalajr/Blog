4277515c3c482c15e1bed82d49e0311edde2cab6
Omar Derbala <derbalajr@gmail.com>
2024-03-12 01:28:34 +0200

    update views

---
+++ resources/views/post.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea ex, delectus iusto placeat nobis facere vero
+            fuga aut quae, iure, molestiae voluptatibus earum. Veritatis sunt similique eius aspernatur provident aut.
+        </p>
+    </article>
+    <a href="/">Go Back</a>
+</body>
